90a2714d7bb67566556188ba3ef302f24a3a49b3
Shreya Verma <72500761+shreya-v7@users.noreply.github.com>
2021-05-31 18:58:26 +0530

    Add files via upload

---
+++ Python-GreenMask-OpenCV-main/Python-GreenMask-OpenCV-main/test.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("image.jpg")
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+mask = cv2.inRange(hsv, (36, 25, 25), (70, 255,255))
+
+# green mask to slice out that part of the image
+imask = mask>0
+green = np.zeros_like(img, np.uint8)
+green[imask] = img[imask]
+
+#displaying in different windows
+cv2.imshow("green-masked", green)
+cv2.imshow("original", img)
