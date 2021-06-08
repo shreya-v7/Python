de64a2df2b681eb06a83bedbe606da301b1bb86e
Shreya Verma <72500761+shreya-v7@users.noreply.github.com>
2021-06-08 23:57:57 +0530

    Update test.py

--- Python-GreenMask-OpenCV-main/Python-GreenMask-OpenCV-main/test.py
+++ Python-GreenMask-OpenCV-main/Python-GreenMask-OpenCV-main/test.py
@@ -5,6 +5,7 @@ img = cv2.imread("image.jpg")
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+# range of color given
 mask = cv2.inRange(hsv, (36, 25, 25), (70, 255,255))
 
 # green mask to slice out that part of the image
